dc239728facd784e909df7e2ac6e07af05ef6196
unknown <dreamconsolestar@gmail.com>
2022-09-14 14:16:39 +0900

    index

---
+++ indexcredits_cc.js
@@ -0,0 +1,21 @@
+
+var modal = document.getElementById("creditsMenu");
+var btn = document.getElementById("creditsBtn");
+var span = document.getElementsByClassName("closeCredits")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
